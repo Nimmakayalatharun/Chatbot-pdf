5c89a3d221a5ccddf55f12d43654b0237c51eb3d
Nimmakayalatharun <117099850+Nimmakayalatharun@users.noreply.github.com>
2023-10-26 17:54:25 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        pdf = request.files['pdf']
+        if pdf:
+            pdf_reader = PdfReader(pdf)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000,
+                chunk_overlap=200,
+                length_function=len
+            )
+            chunks = text_splitter.split_text(text=text)
+
+            store_name = pdf.filename[:-4]
+            if os.path.exists(f"{store_name}.pkl"):
+                with open(f"{store_name}.pkl", "rb") as f:
+                    VectorStore = pickle.load(f)
+            else:
+                embeddings = OpenAIEmbeddings(openai_api_key="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")  ## dummyUse the API key
+                VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+                with open(f"{store_name}.pkl", "wb") as f:
+                    pickle.dump(VectorStore, f)
+
+            query = request.form.get('query')
+            if query:
+                docs = VectorStore.similarity_search(query=query, k=3)
+
+                llm = OpenAI()
+                chain = load_qa_chain(llm=llm, chain_type="stuff")
+                with get_openai_callback() as cb:
+                    response = chain.run(input_documents=docs, question=query)
+                    print(cb)
+                return response
+
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+  
+    
